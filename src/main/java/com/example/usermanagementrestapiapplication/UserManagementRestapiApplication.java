15e6e2df69440223b8ccbfae7d8eb8d453a06a3f
Rains97 <hvinhtuong@gmail.com>
2024-04-12 19:00:34 +0700

    Init project User managment Rest api application

---
+++ src/main/java/com/example/usermanagementrestapiapplication/UserManagementRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.usermanagementrestapiapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementRestapiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/usermanagementrestapiapplication/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.example.usermanagementrestapiapplication.controller;
+
+public class UserController {
+}
